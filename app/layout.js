7d96294383de27ddc95e2320797388eb32e32965
Courtsplayground <adambliss79@gmail.com>
2026-05-20 10:43:12 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "ThreadHub",
+  description: "Community discussion platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
